e92f671ccff06fa3e3bd354bbc546c8f6f632912
dongju park <pdj95044@gmail.com>
2024-06-13 14:06:35 +0900

    입력에 따른 리스트 마지막 요소 삭제 및 리스트 초기화~

--- Server/main.cpp
+++ Server/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <cstring>
-#include<list>
+#include <list>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include "common.hpp"
@@ -13,7 +13,7 @@ using namespace std;
 
 Ball *createBall()
 {
-    //랜덤으로 x, y, dx, dy 생성
+    // 랜덤으로 x, y, dx, dy 생성
     Ball *ball = new Ball;
     ball->pos.x = rand() % 1281;
     ball->pos.y = rand() % 801;
@@ -23,12 +23,13 @@ Ball *createBall()
     return ball;
 }
 
-int main() {
+int main()
+{
     int server_fd, new_socket;
     struct sockaddr_in address;
     int opt = 1;
     int addrlen = sizeof(address);
-    list<Ball> ballList;
+    list<Ball *> ballList;
     Ball *newBall;
     // char buffer[BUFFER_SIZE] = {0};
     char cmd[100];
@@ -36,69 +37,92 @@ int main() {
     const char *hello = "Hello from server";
 
     // 소켓 파일 디스크립터 생성
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
 
     // 소켓 옵션 설정
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
+    {
         perror("setsockopt");
         exit(EXIT_FAILURE);
     }
-    
+
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
 
     // 소켓과 포트를 바인딩
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
+    {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
     // 연결을 대기 상태로 설정
-    if (listen(server_fd, 3) < 0) {
+    if (listen(server_fd, 3) < 0)
+    {
         perror("listen");
         exit(EXIT_FAILURE);
     }
     // 수신 연결 수락
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) {
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0)
+    {
         perror("accept");
         exit(EXIT_FAILURE);
     }
-    while (1) {
+    while (1)
+    {
         memset(cmd, 0, BUFFER_SIZE); // 버퍼 초기화
         int valread = recv(new_socket, cmd, BUFFER_SIZE, 0);
-        if (valread <= 0) {
+        if (valread <= 0)
+        {
             break; // 연결이 끊어지거나 에러 발생 시 루프 탈출
         }
-        switch (cmd[0]) 
+        switch (cmd[0])
         {
-        case 'a':  
-            //공 객체 생성
+        case 'a':
+            // 공 객체 생성
             newBall = createBall();
 
-            //공 객체를 리스트에 추가
-            ballList.push_back(*newBall);
+            // 공 객체를 리스트에 추가
+            ballList.push_back(newBall);
 
             cout << "ballList size: " << ballList.size() << endl;
-            for(list<Ball>::iterator it = ballList.begin(); it != ballList.end(); it++)
+            for (auto it = ballList.begin(); it != ballList.end(); ++it)
             {
-                cout << "x: " << it->pos.x << " y: " << it->pos.y << endl;
+                std::cout << "Ball Position: " << (*it)->pos.x << ", " << (*it)->pos.y
+                          << " | Speed: " << (*it)->speed.dx << ", " << (*it)->speed.dy << std::endl;
             }
-
             break;
         case 'd':
-            
+            // 리스트에서 마지막 객체 삭제
+            if (!ballList.empty())
+            {
+                delete ballList.back();
+                ballList.pop_back();
+            }
+            cout << "ballList size: " << ballList.size() << endl;
+            for (auto it = ballList.begin(); it != ballList.end(); ++it)
+            {
+                std::cout << "Ball Position: " << (*it)->pos.x << ", " << (*it)->pos.y
+                          << " | Speed: " << (*it)->speed.dx << ", " << (*it)->speed.dy << std::endl;
+            }
             break;
         case 'c':
-            
+            // 리스트 초기화
+            // 리스트 내 모든 객체 할당 해제
+            for (auto it = ballList.begin(); it != ballList.end(); ++it)
+            {
+                delete *it;
+            }
+            ballList.clear();
+            cout << "ballList size: " << ballList.size() << endl;
             break;
         default:
             break;
         }
-        
-        
     }
 
     close(new_socket);
